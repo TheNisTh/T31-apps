bf6cd611854e974d599bc7238a512a1efbdd41dc
TheNisTh <denis_thielke@hotmail.com>
2020-08-28 13:19:14 -0400

    alterações visuais

---
+++ app-consumocomb/comb.js
@@ -0,0 +1,11 @@
+function calcular() {
+    //fazer a tela
+
+    //ler os dados da tela(preço alcool e gasolina, consumo do alcool e gasolina)
+
+    //calcular o combustível mais viável
+
+
+    //exibir resultado
+
+}
